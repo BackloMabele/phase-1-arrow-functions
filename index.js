0743645e74d4c659ddaf49d6cf3ffa0f003f104b
BackloMabele <backlo.mabele@student.moringaschool.com>
2023-09-19 17:38:09 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+function divide() {
+    const dividend = 2000
+    const divisor = 100
+    const result = dividend/divisor
+    return result
+}
+const square = x => {
+        return x * x;
+}
+const add = (a, b) => {
+    return a + b;
+}
+
